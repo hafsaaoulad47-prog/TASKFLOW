8df8b07c0723f042372fc78c438c9d2fbabff3e9
hafsaaoulad47-prog <hafsaaoulad47@gmail.com>
2026-05-20 18:54:58 +0100

    push all structure of our taskflow

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+//test route
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is working!" });
+});
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/projects", require("./routes/projects"));
+app.use("/api/tasks", require("./routes/tasks"));
+app.use("/api/dashboard", require("./routes/dashboard"));
+app.use("/api/notifications", require("./routes/notifications"));
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+  app.listen(process.env.PORT, "0.0.0.0",() =>
+  console.log(`🚀 Server running on port ${process.env.PORT}`)
+  );
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  });
